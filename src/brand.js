5b21535a2988a240f95099f589e1216518eed16a
Boricua-Coder <lesther312@gmail.com>
2026-04-05 10:37:58 -0400

    Fix source link target to BC-Blocks repository

--- src/brand.js
+++ src/brand.js
@@ -1,3 +1,3 @@
 export const BRAND_NAME = 'BC-Blocks';
-export const BRAND_REPO_URL = 'https://github.com/lestherruiz';
+export const BRAND_REPO_URL = 'https://github.com/nemesis312/bc-blocks';
 export const BRAND_TAILWIND_CDN = 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4';
